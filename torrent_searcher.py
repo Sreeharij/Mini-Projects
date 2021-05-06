02354f7c6015d05375e109be9c7b9fb2f5b99832
Sreeharij <Sreeharij2003@gmail.com>
2021-05-06 18:08:48 +0530

    Add files via upload

---
+++ torrent_searcher.py
@@ -0,0 +1,80 @@
+def install():
+	try:
+		import requests
+	except:
+		os.sytem('pip install requests')
+	try:
+		from bs4 import BeautifulSoup
+	except:
+		os.sytem('pip install bs4')
+install()
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+path=os.getcwd()
+
+def piratesbay(query):
+	query = query.replace(' ','%20')
+	proxies_site_list = 'https://piratebayproxy.info/'
+	source = requests.get(proxies_site_list).text
+	soup = BeautifulSoup(source,'lxml')
+	proxies_list = []
+
+	for line in soup.find_all('td',class_='site'):
+		site = 'https:\\'+line.a.text
+		proxies_list.append(site)
+	current_proxy = proxies_list[0]
+	api_endpoint = f'/search/{query}/1/99/0'
+	command = f"cd {path} && start {current_proxy+api_endpoint}"
+	print(command)
+	os.system(command)
+
+def limetorrents(query):
+	query = query.replace(' ','-')
+	proxies_list = ['https://www.limetorrents.info/','https://limetorrents.co/']
+	current_proxy = proxies_list[0]
+	api_endpoint = f'search/all/{query}/'
+	
+	command = f"cd {path} && start {current_proxy+api_endpoint}"
+	print(command)
+	os.system(command)
+
+def torrent_1337x(query):
+	query = query.replace(' ','+')
+	proxies_list = ['https://1337x.to/','https://1337xto.to/','https://1337x.gd/']
+	current_proxy = proxies_list[0]
+	api_endpoint = f'search/{query}/1/'
+	
+	command = f"cd {path} && start {current_proxy+api_endpoint}"
+	print(command)
+	os.system(command)
+
+def rarbg(query):
+	query = query.replace(' ','+')
+	proxies_list = ['https://rarbgprx.org/']
+	current_proxy = proxies_list[0]
+	api_endpoint = f'torrents.php?search={query}'
+	
+	command = f"cd {path} && start {current_proxy+api_endpoint}"
+	print(command)
+	os.system(command)
+
+def kickass_torrents(query):
+	query = query.replace(' ','+')
+	proxies_list = ['https://kickasstorrentcr.top/'] 
+	current_proxy = proxies_list[0]
+	api_endpoint = f'search.php?q={query}&cat=0'
+
+	command = f"cd {path} && start {current_proxy+api_endpoint}"
+	print(command)
+	os.system(command)
+
+query = input("Enter torrent to be searched: ")
+piratesbay(query)
+limetorrents(query)
+kickass_torrents(query)
+torrent_1337x(query)
+rarbg(query)
+input()
